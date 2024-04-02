c401c24fa6bbff3a801a7b74a6427443f6ecfb69
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-03 01:00:21 +0200

    Changing install.sql

--- install/db/install.sql
+++ install/db/install.sql
@@ -1,15 +1,12 @@
 CREATE TABLE up_subscription(
 	                            ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-	                            LEVEL ENUM('Free', 'Standard', 'Premium') DEFAULT 'Free',
+	                            LEVEL   VARCHAR(50) DEFAULT 'Free',
 	                            PRICE INT NOT NULL DEFAULT 0,
-	                            NUMBER_TREES INT NOT NULL DEFAULT 1
+	                            NUMBER_TREES INT(3) NOT NULL DEFAULT 1
 );
 
-CREATE TABLE up_user(
-	                    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	                    NAME VARCHAR(50) NOT NULL,
-	                    EMAIL VARCHAR(100) NOT NULL,
-	                    PASSWORD_HASH VARCHAR(255) NOT NULL,
+CREATE TABLE up_user_subscription(
+                        USER_ID INT NOT NULL,
 	                    SUBSCRIPTION_ID INT NOT NULL DEFAULT 1,
 	                    SUBSCRIPTION_BUY_TIME TIMESTAMP DEFAULT NULL
 );
@@ -40,22 +37,16 @@ CREATE TABLE up_relation_person_parent(
 
 CREATE TABLE up_publication(
 	                           ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	                           USER_ID INT NOT NULL,
-	                           MESSAGE TEXT NOT NULL
+	                           AUTHOR_ID INT NOT NULL,
+	                           MESSAGE TEXT NOT NULL,
+	                           CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TABLE up_comment(
 	                       ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	                       PARENT_ID INT DEFAULT NULL,
 	                       PUBLICATION_ID INT NOT NULL,
-	                       USER_ID INT NOT NULL,
-	                       COMMENT TEXT NOT NULL
+	                       AUTHOR_ID INT NOT NULL,
+	                       COMMENT TEXT NOT NULL,
+	                       CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE up_relation_comment_parent(
-	                                       PARENT_ID INT NOT NULL,
-	                                       CHILD_ID INT NOT NULL,
-	                                       PRIMARY KEY (PARENT_ID, CHILD_ID)
-);
-
