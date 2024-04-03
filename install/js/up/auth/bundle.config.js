dac6c7cea9a3860cc1d98d736d94975e72a3ecfc
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-04-03 21:27:27 +0200

    Adding components for register

---
+++ install/js/up/auth/bundle.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	input: 'src/auth.js',
+	output: 'dist/auth.bundle.js',
+	namespace: 'BX.Up.Tree',
+	sourceMaps: false,
+};
---
+++ install/js/up/auth/src/auth.js
@@ -0,0 +1,10 @@
+import {Type} from 'main.core';
+
+export class Auth
+{
+	constructor(options = {})
+	{
+		console.log('test');
+	}
+
+}
