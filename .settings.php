50f156d48c807f55c3bf1c21c2f8c157699ed44d
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-04-03 15:16:57 +0200

    Adding .settings.php

---
+++ .settings.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'controllers' => [
+        'value' => [
+            'namespaces' => [
+                '\\Up\\Tree\\Controller' => 'api',
+            ],
+            'defaultNamespace' => '\\Up\\Tree\\Controller',
+        ],
+        'readonly' => true,
+    ],
+];
