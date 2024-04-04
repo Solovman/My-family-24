becc511812cec08b51ca772a3f2ef1f1bf4c7616
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-05 00:35:26 +0200

    Removing the search string in the header

--- install/templates/tree/header.php
+++ install/templates/tree/header.php
@@ -111,14 +111,6 @@ use Up\Tree\Services\Repository\UserService;
 	</nav>
 	<div class="container">
 		<div class="header__main">
-			<form class="header__search-form">
-				<input class="header__input-search" placeholder="<?= GetMessage('UP_HEADER_PLACEHOLDER_SEARCH')?>" type="text">
-				<button class="header__button-search" type="submit">
-					<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</button>
-			</form>
 			<div class="header__icon">
 				<img class="header__image-icon" src="/local/modules/up.tree/images/user_default.png" alt="user-icon">
 				<span class="header__icon-name"><?= UserService::getUserNameById() ?></span>
