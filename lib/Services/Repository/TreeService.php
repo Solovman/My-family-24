fe06736a0f2158798f15c7c50556aaf2c19a31e7
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-05 17:47:25 +0200

    Added the ability to obtain all information about a tree by its id on the backend

--- lib/Services/Repository/TreeService.php
+++ lib/Services/Repository/TreeService.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Up\Tree\Services\Repository;
 
-use Bitrix\Main\DB\SqlException;
 use Exception;
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\DB\SqlException;
+use Bitrix\Main\ObjectException;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
+use Bitrix\Main\Type\DateTime;
 use Up\Tree\Entity\Tree;
 use Up\Tree\Model\TreeTable;
 
@@ -30,5 +35,56 @@ class TreeService
 		}
 
 		throw new SqlException("Error creating tree");
+	}
+
+	/**
+	 * @throws ArgumentException
+	 * @throws ObjectException
+	 * @throws ObjectPropertyException
+	 * @throws SystemException
+	 */
+	public static function getTreeByUserId(int $userId): ?Tree
+	{
+		$treeData = TreeTable::query()
+							 ->setSelect(['ID', 'TITLE', 'USER_ID', 'CREATED_AT'])
+							 ->setFilter(['USER_ID' => $userId])
+							 ->exec()
+							 ->fetch();
+
+		if (!$treeData)
+		{
+			return null;
+		}
+
+		$tree = new Tree(
+			$treeData['TITLE'],
+			(int)$treeData['USER_ID'],
+			new DateTime($treeData['CREATED_AT'])
+		);
+		$tree->setId((int)$treeData['ID']);
+
+		// Getting tree personas for tree
+		$persons = PersonService::getPersonsByTreeId($tree->getId());
+
+		foreach ($persons as $person)
+		{
+			$tree->addPerson($person);
+		}
+
+		$personIds = [];
+		foreach ($tree->getPersons() as $person)
+		{
+			$personIds[] = $person->getId();
+		}
+
+		// Getting relationships between persons for tree
+		$relations = FamilyRelationService::getFamilyRelationByPersonsIds($personIds);
+
+		foreach ($relations as $relation)
+		{
+			$tree->addFamilyRelation($relation);
+		}
+
+		return $tree;
 	}
 }
