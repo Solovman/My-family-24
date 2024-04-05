03263ff1e4328761d7ec9acfba642fadde5550df
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-04-05 13:46:29 +0200

    Adding image

--- install/js/up/creationtree/dist/creationTree.bundle.js
+++ install/js/up/creationtree/dist/creationTree.bundle.js
@@ -39,26 +39,22 @@ this.BX.Up = this.BX.Up || {};
 	        },
 	        nodeBinding: {
 	          field_0: "name",
-	          field_1: "title",
 	          img_0: "img"
 	        }
 	      });
 	      chart.load([{
 	        id: 1,
 	        name: "Denny Curtis",
-	        title: "CEO",
 	        img: "https://cdn.balkan.app/shared/2.jpg"
 	      }, {
 	        id: 2,
 	        pid: 1,
 	        name: "Ashley Barnett",
-	        title: "Sales Manager",
 	        img: "https://cdn.balkan.app/shared/3.jpg"
 	      }, {
 	        id: 3,
 	        pid: 1,
 	        name: "Caden Ellison",
-	        title: "Dev Manager",
 	        img: "https://cdn.balkan.app/shared/4.jpg"
 	      }]);
 	    }
