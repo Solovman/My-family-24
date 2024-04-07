02e721e2c48e231dd5ad7e34bf5ea6fcbe7d5b82
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-07 18:59:37 +0200

    Added the ability to add a new family member with any type of relationship

--- lib/Services/Repository/PersonService.php
+++ lib/Services/Repository/PersonService.php
@@ -10,14 +10,21 @@ use Bitrix\Main\ObjectPropertyException;
 use Bitrix\Main\SystemException;
 use Exception;
 use Up\Tree\Entity\Person;
+use Up\Tree\Model\MarriedTable;
+use Up\Tree\Model\PersonParentTable;
 use Up\Tree\Model\PersonTable;
 
 class PersonService
 {
 	/**
 	 * @throws SqlException
+	 * @throws Exception
 	 */
-	public static function addPerson(Person $person): int|array
+	public static function addPerson(
+		Person $person,
+		int    $personConnectedId,
+		string $relationType
+	): array
 	{
 		$personData = [
 			"IMAGE_ID" => $person->getImageId(),
@@ -29,13 +36,58 @@ class PersonService
 			"TREE_ID" => $person->getTreeId(),
 		];
 
-		$result = PersonTable::add($personData);
-		if ($result->isSuccess())
+		$ids = [];
+
+		$newPerson = PersonTable::add($personData);
+
+		if (!$newPerson->isSuccess())
 		{
-			return $result->getId();
+			throw new SqlException("Error when adding person");
 		}
 
-		throw new SqlException("Error when adding person");
+		$personId = $newPerson->getId();
+		$ids[] = $personId;
+
+		if ($relationType === 'partner')
+		{
+			$relationMarriedData = [
+				"PERSON_ID" => $newPerson->getId(),
+				"PARTNER_ID" => $personConnectedId,
+			];
+
+			$relationMarriedDataReverse = [
+				"PERSON_ID" => $personConnectedId,
+				"PARTNER_ID" => $newPerson->getId(),
+			];
+
+			$relationMarriedId = MarriedTable::add($relationMarriedData)->getId();
+			$relationMarriedReverseId = MarriedTable::add($relationMarriedDataReverse)->getId();
+
+			$ids[] = $relationMarriedId;
+			$ids[] = $relationMarriedReverseId;
+		}
+		if ($relationType === 'parent')
+		{
+			$relationData = [
+				"PARENT_ID" => $newPerson->getId(),
+				"CHILD_ID" => $personConnectedId,
+			];
+
+			$relationId = PersonParentTable::add($relationData)->getId();
+			$ids[] = $relationId;
+		}
+		if ($relationType === 'child')
+		{
+			$relationData = [
+				"PARENT_ID" => $personConnectedId,
+				"CHILD_ID" => $newPerson->getId(),
+			];
+
+			$relationId = PersonParentTable::add($relationData)->getId();
+			$ids[] = $relationId;
+		}
+
+		return $ids;
 	}
 
 	/**
@@ -45,18 +97,16 @@ class PersonService
 	 */
 	public static function getPersonsByTreeId(int $treeId): array
 	{
-		$persons = PersonTable::query()
-							  ->setSelect(['ID',
-										   'IMAGE_ID',
-										   'NAME',
-										   'SURNAME',
-										   'BIRTH_DATE',
-										   'DEATH_DATE',
-										   'GENDER',
-										   'TREE_ID'])
-							  ->setFilter(['TREE_ID' => $treeId])
-							  ->exec()
-							  ->fetchAll();
+		$persons = PersonTable::query()->setSelect([
+													   'ID',
+													   'IMAGE_ID',
+													   'NAME',
+													   'SURNAME',
+													   'BIRTH_DATE',
+													   'DEATH_DATE',
+													   'GENDER',
+													   'TREE_ID',
+												   ])->setFilter(['TREE_ID' => $treeId])->exec()->fetchAll();
 
 		$personList = [];
 
