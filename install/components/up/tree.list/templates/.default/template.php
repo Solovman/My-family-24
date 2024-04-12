0b6bb54847207d7573696270cd44512a7945146d
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-12 16:39:20 +0200

    Fonts have been enlarged

--- install/components/up/tree.list/templates/.default/template.php
+++ install/components/up/tree.list/templates/.default/template.php
@@ -14,13 +14,13 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 		<div class="column">
 			<div class="field">
 				<div class="control" style="position: relative">
-					<div id="warningMessage" style="color: #ff6068; position: absolute; bottom: 100%; left: 0"></div>
-					<input class="input input-title-tree" type="text" placeholder="Enter a tree title" id="treeTitleInput" name="treeTitle" required>
+					<div id="warningMessage" style="color: #ff6068; position: absolute; bottom: 100%; left: 0; font-size: 1.5em"></div>
+					<input class="input input-title-tree" type="text" placeholder="Enter a tree title" id="treeTitleInput" name="treeTitle" style="font-size: 1.5em" required>
 				</div>
 			</div>
 		</div>
 		<div class="column">
-			<button type="button" class="button is-success is-rounded button-add-tree" id="addTreeButton">Create new tree!</button>
+			<button type="button" class="button is-success is-rounded button-add-tree" id="addTreeButton" style="font-size: 1.5em">Create new tree!</button>
 		</div>
 	</div>
 </form>
