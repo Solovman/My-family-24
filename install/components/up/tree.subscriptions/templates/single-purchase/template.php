d56d00e6a72988b7ff9ce68f6510f547e2e051db
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-04-19 19:43:13 +0200

    Adding tooltips

--- install/components/up/tree.subscriptions/templates/single-purchase/template.php
+++ install/components/up/tree.subscriptions/templates/single-purchase/template.php
@@ -4,12 +4,17 @@
 			<div class="article-card">
 				<div class="content">
 					<p class="title">Sriniz Template</p>
-					<button class="subscriptions__purchase-button">
-						<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-						<span>Купить</span>
-					</button>
+					<div class="field filed-purchase">
+						<button class="subscriptions__purchase-button has-tooltip">
+							<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							<span>Купить</span>
+						</button>
+						<span class='tooltip blue tooltip-purchase'>
+							<p class="tooltip-p">Для покупки обратитесь к нашему <a href="mailto:familyTreeTechnicalSupport@gmail.com">менеджеру</a></p>
+						</span>
+					</div>
 				</div>
 				<img src="/local/modules/up.tree/images/template_1.png" alt="template_1" />
 			</div>
@@ -20,12 +25,17 @@
 			<div class="article-card">
 				<div class="content">
 					<p class="title">Multiple Templates Family</p>
-					<button class="subscriptions__purchase-button">
-						<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-						<span>Купить</span>
-					</button>
+					<div class="field filed-purchase">
+						<button class="subscriptions__purchase-button has-tooltip">
+							<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							<span>Купить</span>
+						</button>
+						<span class='tooltip blue tooltip-purchase'>
+							<p class="tooltip-p">Для покупки обратитесь к нашему <a href="mailto:familyTreeTechnicalSupport@gmail.com">менеджеру</a></p>
+						</span>
+					</div>
 				</div>
 				<img src="/local/modules/up.tree/images/template_2.png" alt="template_2"/>
 			</div>
@@ -36,12 +46,17 @@
 			<div class="article-card">
 				<div class="content">
 					<p class="title">Royal Family Tree</p>
-					<button class="subscriptions__purchase-button">
-						<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-						<span>Купить</span>
-					</button>
+					<div class="field filed-purchase">
+						<button class="subscriptions__purchase-button has-tooltip">
+							<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							<span>Купить</span>
+						</button>
+						<span class='tooltip blue tooltip-purchase'>
+							<p class="tooltip-p">Для покупки обратитесь к нашему <a href="mailto:familyTreeTechnicalSupport@gmail.com">менеджеру</a></p>
+						</span>
+					</div>
 				</div>
 				<img src="/local/modules/up.tree/images/template_3.png" alt="template_2" />
 			</div>
@@ -52,12 +67,17 @@
 			<div class="article-card">
 				<div class="content">
 					<p class="title">Dark Theme</p>
-					<button class="subscriptions__purchase-button">
-						<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-						<span>Купить</span>
-					</button>
+					<div class="field filed-purchase">
+						<button class="subscriptions__purchase-button has-tooltip">
+							<svg class="subscriptions__purchase-svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							<span>Купить</span>
+						</button>
+						<span class='tooltip blue tooltip-purchase'>
+							<p class="tooltip-p">Для покупки обратитесь к нашему <a href="mailto:familyTreeTechnicalSupport@gmail.com">менеджеру</a></p>
+						</span>
+					</div>
 				</div>
 				<img src="/local/modules/up.tree/images/dark-theme.png" alt="dark-theme" />
 			</div>
--- install/js/up/subscriptions/src/subscriptions.js
+++ install/js/up/subscriptions/src/subscriptions.js
@@ -128,19 +128,30 @@ export class Subscriptions
 			const card = Tag.render`
 			<li class="subscriptions__item">
 				<div class="nft ntf_${list.id}">
-				<div class='main'>
-					<h2 class="subscriptions__heading">${this.getEmojiByTitle(list.level)}</h2>
-					<p class='description'></p>
+					<div class='main'>
+						<h2 class="subscriptions__heading">${this.getEmojiByTitle(list.level)}</h2>
+						<p class='description'></p>
+					</div>
+					<ul style="color: white; font-size: 1.4em">
+						<li>✧ Цена: ${list.price}$</li>
+						<li>✧ ${this.getCountTreesMessage(list.numberTrees)}</li>
+						<li>✧ ${this.getCountNodesMessage(list.numberNodes)}</li>
+						<li>✧ ${this.getCustomStatusMessage(list.customization)}</li>
+						<li>✧ ${this.getSubscriptionStatusMessage(list.subscriptionType)}</li>
+					</ul>
+					
+					<div class="field">
+						<button id="${list.level}${list.id}" class="subscriptions__button has-tooltip">
+							<svg class="subscriptions__main-svg" width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+							</svg>
+							<span class="subscriptions__span">Купить</span>
+						</button>
+						<span class='tooltip blue'>
+							<p class="tooltip-p">Для покупки обратитесь к нашему <a href="mailto:familyTreeTechnicalSupport@gmail.com">менеджеру</a></p>
+						</span>
+					</div>
 				</div>
-				<ul style="color: white; font-size: 1.4em">
-					<li>✧ Цена: ${list.price}$</li>
-					<li>✧ ${this.getCountTreesMessage(list.numberTrees)}</li>
-					<li>✧ ${this.getCountNodesMessage(list.numberNodes)}</li>
-					<li>✧ ${this.getCustomStatusMessage(list.customization)}</li>
-					<li>✧ ${this.getSubscriptionStatusMessage(list.subscriptionType)}</li>
-				</ul>
-				<button id="${list.level}${list.id}" class="subscriptions__button">Купить</button>
-			</div>
 			</li>
 		`;
 
