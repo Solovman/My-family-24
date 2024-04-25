69b91eeb648ef43853831363648224fac0a50da0
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-25 22:43:10 +0200

    Update uninstall db file

--- install/db/uninstall.sql
+++ install/db/uninstall.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `up_relation_user_subscription`, `up_comment`, `up_publication`,
+DROP TABLE IF EXISTS `up_relation_user_subscription`, `up_chat`, `up_message`,
 	`up_image`, `up_relation_person_parent`, `up_person`,
 	`up_family_tree`, `up_user`, `up_subscription`, `up_relation_married`,
 	`up_relation_user_single_purchase`, `up_single_purchase`;
