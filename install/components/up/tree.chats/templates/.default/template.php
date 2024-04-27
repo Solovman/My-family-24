41049b3e26e6b3f78de17c33d035af4856b13bd0
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-04-27 23:44:31 +0200

    Adding new chat

--- install/components/up/tree.chats/templates/.default/template.php
+++ install/components/up/tree.chats/templates/.default/template.php
@@ -10,10 +10,24 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 
 <div class="my-container">
-	<section class="section">
+	<div class="section">
 		<h1 class="title">Список чатов:</h1>
-		<div class="container" id="chat-container"></div>
-	</section>
+		<div class="chat__container">
+			<div class="discussions" id="chat-container"></div>
+			<div class="chat">
+				<div class="header-chat">
+					<i class="icon fa fa-user-o" aria-hidden="true"></i>
+					<p id="name-user" class="name"></p>
+					<i class="icon clickable fa fa-ellipsis-h right" aria-hidden="true"></i>
+				</div>
+				<div id="messages" class="messages-chat"></div>
+				<div class="footer-chat">
+					<input id="input-message" type="text" class="write-message" placeholder="Type your message here">
+					<div id="footer-send"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <script>
@@ -21,6 +35,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 	{
 		const chat = new BX.Up.Tree.Chat({
 			rootNodeId : 'chat-container',
+			rootMessages: 'messages'
 		});
 	});
 </script>
