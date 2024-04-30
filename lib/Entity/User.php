e5bf63209ffa3354f4f9625512a2935d6c9dcf10
Илья  Соловьев <solovevilya3101@gmail.com>
2024-04-30 18:55:00 +0200

    Fixing a bug with login devices in the mail field

--- lib/Entity/User.php
+++ lib/Entity/User.php
@@ -7,20 +7,20 @@ namespace Up\Tree\Entity;
 class User
 {
 	public int $id;
-	public string $login;
+	public string $email;
 	public string $name;
 	public string $lastName;
 	public static $active;
 	public function __construct(
 		$id,
-		$login,
+		$email,
 		$name,
 		$lastName,
 		$active
 	)
 	{
 		$this->id = $id;
-		$this->login = $login;
+		$this->email = $email;
 		$this->name = $name;
 		$this->lastName = $lastName;
 		$this->active = $active;
