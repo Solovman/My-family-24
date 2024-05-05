a82434d43b598b768194c908bc12e50b7f53c2b6
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-05-05 11:13:08 +0200

    Autocomplete off

--- install/templates/noauth/components/bitrix/system.auth.registration/.default/template.php
+++ install/templates/noauth/components/bitrix/system.auth.registration/.default/template.php
@@ -49,7 +49,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 										<path d="M20 17.5C20 19.9853 20 22 12 22C4 22 4 19.9853 4 17.5C4 15.0147 7.58172 13 12 13C16.4183 13 20 15.0147 20 17.5Z" stroke="#ff6068" stroke-width="1.5"/>
 									</svg>
 								</span>
-								<input placeholder="<?= GetMessage('REGISTER_PLACEHOLDER_NANE') ?>" type="text" name="USER_NAME" maxlength="50" value="<?=$arResult["USER_NAME"]?>" class="bx-auth-input main__input-form-reg" />
+								<input autocomplete="off" placeholder="<?= GetMessage('REGISTER_PLACEHOLDER_NANE') ?>" type="text" name="USER_NAME" maxlength="50" value="<?=$arResult["USER_NAME"]?>" class="bx-auth-input main__input-form-reg" />
 							</td>
 						</tr>
 						<tr>
@@ -61,7 +61,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 										<path d="M20 17.5C20 19.9853 20 22 12 22C4 22 4 19.9853 4 17.5C4 15.0147 7.58172 13 12 13C16.4183 13 20 15.0147 20 17.5Z" stroke="#ff6068" stroke-width="1.5"/>
 									</svg>
 								</span>
-								<input type="text" placeholder="<?= GetMessage('REGISTER_PLACEHOLDER_SURNAME') ?>" name="USER_LAST_NAME" maxlength="50" value="<?=$arResult["USER_LAST_NAME"]?>" class="bx-auth-input main__input-form-reg" />
+								<input autocomplete="off" type="text" placeholder="<?= GetMessage('REGISTER_PLACEHOLDER_SURNAME') ?>" name="USER_LAST_NAME" maxlength="50" value="<?=$arResult["USER_LAST_NAME"]?>" class="bx-auth-input main__input-form-reg" />
 							</td>
 						</tr>
 						<?php if($arResult["EMAIL_REGISTRATION"]):?>
@@ -74,7 +74,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 											<path d="m458.15 617.7 18.8-107.3a56.94 56.94 0 0 1 35.2-101.9V289.4h-145.2a56.33 56.33 0 0 0-56.3 56.3v275.8a33.94 33.94 0 0 0 3.4 15c12.2 24.6 60.2 103.7 197.9 164.5V622.1a313.29 313.29 0 0 1-53.8-4.4zM656.85 289h-144.9v119.1a56.86 56.86 0 0 1 35.7 101.4l18.8 107.8A320.58 320.58 0 0 1 512 622v178.6c137.5-60.5 185.7-139.9 197.9-164.5a33.94 33.94 0 0 0 3.4-15V345.5a56 56 0 0 0-16.4-40 56.76 56.76 0 0 0-40.05-16.5z" style="fill:#fff"/>
 										</svg>
 									</span>
-									<input class="bx-auth-input main__input-form-reg" placeholder="<?= GetMessage('REGISTER_PLACEHOLDER_EMAIL') ?>" type="text" name="USER_EMAIL" maxlength="255" value="<?=$arResult["USER_EMAIL"]?>" class="bx-auth-input" />
+									<input autocomplete="off" class="bx-auth-input main__input-form-reg" placeholder="<?= GetMessage('REGISTER_PLACEHOLDER_EMAIL') ?>" type="text" name="USER_EMAIL" maxlength="255" value="<?=$arResult["USER_EMAIL"]?>" class="bx-auth-input" />
 								</td>
 							</tr>
 						<?php endif?>
